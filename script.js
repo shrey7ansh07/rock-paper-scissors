21bfe965e2679377fcdce3b688dc0cd0aad00369
shrey7ansh07 <shreyguptaji007@gmail.com>
2023-07-04 17:50:10 +0530

    some typos were corrected and code tested

--- script.js
+++ script.js
@@ -1,47 +1,54 @@
 function rps(userinput,systeminput)
 {
-    switch(toLowerCase(userinput))
+    
+    switch(userinput.toLowerCase())
     {
         case "rock":
-            switch(toLowerCase(systeminput))
+            switch(systeminput.toLowerCase())
             {
-                case "paper":
+                case "scissors":
                     return "user-wins!!";
                     break;
-                case "scissors":
+                case "paper":
                     return "system-wins";
                     break;
                 case "rock":
                     return "draw";
                     break;
             }
-                case "paper":
-                    switch(toLowerCase(systeminput))
-                    {
-                        case "rock":
-                            return "user-wins!!";
-                            break;
-                        case "scissors":
-                            return "system-wins";
-                            break;
-                        case "paper":
-                            return "draw";
-                            break;
-                    }
+            break;
+        case "paper":
+            switch(systeminput.toLowerCase())
+            {
+                case "rock":
+                    return "user-wins!!";
+                    break;
                 case "scissors":
-                    switch(toLowerCase(systeminput))
-                    {
-                        case "paper":
-                            return "user-wins!!";
-                            break;
-                        case "rock":
-                            return "system-wins";
-                            break;
-                        case "scissors":
-                            return "draw";
-                            break;
-                    }
-                
+                    return "system-wins";
+                    break;
+                case "paper":
+                    return "draw";
+                    break;
+            }
+            break;
+        case "scissors":
+            switch(systeminput.toLowerCase())
+            {
+                case "paper":
+                    return "user-wins!!";
+                    break;
+                case "rock":
 
+                    return "system-wins";
+                    break;
+                case "scissors":
+                    return "draw";
+                    break;
+            }
+            break;
+        default:
+            return "enter a valid name";
+                
+        
     }
 }
